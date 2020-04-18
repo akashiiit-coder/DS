208da8c3ea38ecb0fd1dbedf10b6872fc86374ba
Akash Anand <54110130+akashiiit-coder@users.noreply.github.com>
2020-04-18 14:23:40 +0530

    Update i.a.c

--- i.a.c
+++ i.a.c
@@ -1,3 +1,8 @@
+/*************************************************
+            This program was creted
+            and     verified     by
+                AKASH  ANAND
+*************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
